4a95f9aa13996fcdae983eca4ecf0e5432241703
faqihnextai <faqihloh@gmail.com>
2025-05-16 13:34:04 +0700

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const data = { username, password };
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/login', { // Ganti ke http://127.0.0.1:5000
+        const response = await fetch('https://faqih.pythonanywhere.com/login', { // Ganti ke http://127.0.0.1:5000
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
